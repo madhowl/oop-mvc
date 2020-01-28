3676a542e874d1bf308fce1f2964a48748df3612
MadHowl <madhowl@yandex.ru>
2020-01-28 13:16:52 +1000

    добавлен noahbuscher/macaw в качестве роутера

--- index.php
+++ index.php
@@ -1,24 +1,19 @@
 <?php
 require_once ('vendor/autoload.php');
 
-use App\Controller ;
-use App\ServiceController;
+//use App\Controller ;
+//use App\ServiceController;
+use \NoahBuscher\Macaw\Macaw;
 
-$tech =new ServiceController();
-$c = new Controller();
-//$tech->dbg($_SERVER);
-//$tech->dbg($_SERVER['REQUEST_URI']);
-switch ($_SERVER['REQUEST_URI']){
-    case '/all':
-        $c->allNews();
-        break;
-    case '/news/15':
-        $c->singleNews (25);
-        break;
-    default:
-        echo '<h1>!WTF! 404 ERROR !WTF!</h1><p>Страница не найдена - грусть печаль :(</p><p>  Ибо нефиг - может ты мамкин Хацкер </p>';
-}
 
-//$c->allNews ();
-//$c->singleNews (25);
+Macaw::get('/', function() {
+    echo 'Hello world!';
+});
+Macaw::get('/about', function() {
+    echo 'ЭТО страница About !';
+});
 
+Macaw::get('news/', 'App\Controller@allNews');
+Macaw::get('news/(:num)', 'App\Controller@singleNews');
+
+Macaw::dispatch();
