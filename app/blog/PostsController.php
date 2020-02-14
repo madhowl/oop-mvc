6ab4ce82c1fa748300ce2db84758bbe28bd3962f
MadHowl <madhowl@yandex.ru>
2020-02-14 12:49:12 +1000

    add categories

--- app/blog/PostsController.php
+++ app/blog/PostsController.php
@@ -6,24 +6,29 @@ namespace App\blog;
 
 use App\blog\PostsModel as Model;
 use App\blog\PostsView as View;
-use Core\ServiceController as s;
+use Core\ServiceController as S;
 
 class PostsController
 {
-    public $Model;
+    public $Posts;
+    public $Categories;
     public $View;
 
     public function __construct ()
     {
-        $this->Model = new Model('posts');
+        $this->Posts = new Model('posts');
+        $this->Categories = new Model('categories');
         $this->View = new View();
     }
 
     public function getAllPosts()
     {
-        $this->Model->all ();
+        $postList = $this->Posts->all ();
+        $categoriesList = $this->Categories->all ();
+        $lastPost = $this->Posts->getLastPost ();
         //S::dbg ($this->Model->out);
-        $this->View->ShowAll($this->Model->out);
+        //S::dbg ($lastPost);
+        $this->View->ShowAll($postList,$categoriesList, $lastPost);
     }
 
 }
--- app/blog/PostsView.php
+++ app/blog/PostsView.php
@@ -9,9 +9,9 @@ use Core\ServiceController;
 
 class PostsView extends CoreView
 {
-    public function showAll($posts)
+    public function showAll($posts, $categories, $lastPost)
     {
 
-        echo $this->twig->render ('blog/posts.twig',['posts'=>$posts]);
+        echo $this->twig->render ('blog/posts.twig',['posts'=>$posts,'categories'=>$categories, 'lastpost'=>$lastPost]);
     }
 }
